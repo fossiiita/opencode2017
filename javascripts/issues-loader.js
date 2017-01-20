98e6fe59c07854d02f745680844f1b58bfbe1b08
Vamsi Sangam <vamsisangam@live.com>
2017-01-20 21:59:09 +0530

    Modified issues section (#85)

--- javascripts/issues-loader.js
+++ javascripts/issues-loader.js
@@ -1,22 +1,43 @@
+var page = 1;
+var per_page = 8;
+var total_count = 0;
+
 $(document).ready(function() {
-    $.getJSON("https://api.github.com/repos/fossiiita/opencodecollab/issues?state=open", {}, loadIssues);
+    // making a request to get the number of issues
+    $.getJSON("https://api.github.com/search/issues?q=label:opencode+state:open+type:issue&page=1&per_page=1", {}, init);
 });
 
-function loadIssues(data) {
-    $(data).each(function(index) {
+function init(data) {
+    total_count = data.total_count;
+
+    for (i = 1; i <= Math.ceil(total_count / per_page); ++i) {
+        $('#issues-pagination').append('<li id="issues-page-' + i + '"><a href="#issues" onclick="getIssuesForPage(' + i + ')">' + i + '</a></li>');
+    }
+
+    getIssuesForPage(page);
+}
+
+function getIssuesForPage(num) {
+    $('#issues-page-' + page).removeClass('active');
+    page = num;
+    $('#issues-page-' + page).addClass('active');
+    $('#issues-content').empty();
+    loadIssues();
+}
+
+function loadIssues() {
+    $.getJSON("https://api.github.com/search/issues?q=label:opencode+state:open+type:issue&page=" + page + "&per_page=" + per_page, {}, loadIssuesCallback);
+}
+
+function loadIssuesCallback(data) {
+    $(data.items).each(function(index) {
         var issue_url = this.html_url;
         var title = this.title;
         var author = this.user.login;
         var author_url = this.user.html_url;
         var labels = "";
         var comments = this.comments;
-        var issue_type;
-
-        if (this.pull_request) {
-            issue_type = '<a href="' + issue_url + '" target="_tab"><i class="pull-right fa fa-2x fa-code-fork"></i></a>';
-        } else {
-            issue_type = '<a href="' + issue_url + '" target="_tab"><i class="pull-right fa fa-2x fa-exclamation-circle"></i></a>';
-        }
+        var issue_type = '<a href="' + issue_url + '" target="_tab"><i class="pull-right fa fa-2x fa-exclamation-circle"></i></a>';
 
         $(this.labels).each(function(index) {
             var link = 'https://github.com/fossiiita/opencodecollab/issues?q=is:issue is:open label:"' + this.name + '"';
